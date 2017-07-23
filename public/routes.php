acfa7e5656d7aa3b68f002e6967553922d864a58
Dimonmara <komarinskiy@gmail.com>
2017-07-23 19:30:46 +0300

    Добавил отображение расписания (где есть)

--- public/routes.php
+++ public/routes.php
@@ -6,26 +6,39 @@
     // запрашивать базу данных transport для пользователя
     
     $rows = query("SELECT * FROM `transport` WHERE `id` = ?",$_GET["id"]);
+    $k=$_GET["id"];
+    $transport = $rows[0];
     foreach ($rows as $row){
         $front = $row["front"];//список id остановок по маршруту
         $back = $row["back"];//список id остановок по маршруту в обратном направлении
     }
-    //var_dump($front);
+    $mfront = explode(",",$front);
+    $mback = explode(",",$back);
+    unset($rows);
+    unset($row);
+    
     //формируем перечень названий остановок по маршруту
-    $rowsfront = query("SELECT stops_name FROM stops WHERE `id` IN($front)");
-    foreach ($rowsfront as $row){
-        $sumfront[] = $row["stops_name"];//массив с названиями остановок
+    $i=0;
+    foreach ($mfront as $value){
+        $rowsfront = query("SELECT stops_name FROM stops WHERE `id` IN($value)");
+        $sumfront[$i] = $rowsfront[0]["stops_name"];//массив с названиями остановок
+        $i++;
     }
+    //var_dump($sumfront);
+    unset($value);
+    
     $fs_separated = implode(", ",$sumfront);//строка с названиями остановок
     //формируем перечень названий остановок по маршруту
-    $rowsback = query("SELECT stops_name FROM stops WHERE `id` IN($back)");
-    foreach ($rowsback as $row){
-        $sumback[] = $row["stops_name"];//массив с названиями остановок
+    $i=0;
+    foreach ($mback as $value){
+        //var_dump($value);
+        $rowsback = query("SELECT stops_name FROM stops WHERE `id` IN($value)");
+        //var_dump($rowsfront);
+        $sumback[$i] = $rowsback[0]["stops_name"];//массив с названиями остановок
+        $i++;
     }
+    unset($value);
     $bs_separated = implode(", ",$sumback);//строка с названиями остановок
-    //var_dump($bs_separated);
-    $transport = $rows[0];
-
 ?> 
 
 <html>
@@ -40,7 +53,7 @@
 		     <h1 class="tit18">
 		        <?= $transport["type"] ?>&nbsp;№<?= $transport["n_marshr"] ?>
 		     </h1>
-		<p><strong>Описание</strong> |&nbsp;<a href="index.html" title="Вернуться на карту">Схема&nbsp;маршрута</a> |&nbsp;<a href="kniga.php?c=&tip=<?=$transport["type"]?>&nomer=<?=$transport["n_marshr"]?>" title="Отзывы о сервисе на маршруте...">Книга жалоб и предложений</a> |&nbsp;<a href="/city_routes/193/news/" title="Посмотреть все новости, связанные с данным маршрутом...">Новости&nbsp;по&nbsp;теме</a></p>
+		<p><strong>Описание</strong> |&nbsp;<a href="index.html" title="Вернуться на карту">Схема&nbsp;маршрута</a> |&nbsp;<a href="kniga.php?id=<?=$k?>&c=" title="Отзывы о сервисе на маршруте...">Книга жалоб и предложений</a> |&nbsp;<a href="/city_routes/193/news/" title="Посмотреть все новости, связанные с данным маршрутом...">Новости&nbsp;по&nbsp;теме</a></p>
 <div class="tc_main">
 	<table class="under_line" cellspacing="0" cellpadding="3">
 				<tbody><tr><th>Стоимость проезда:</th><td><?= $transport["cena"] ?></td></tr>
@@ -53,15 +66,14 @@
 				
 				<tr><th>Конечные станции:</th><td><?= $transport["nach_kon"] ?></td></tr>
 				
-				<tr><th>Путь следования:</th><td><a href="/city_routes/ks/118/" title="Подробнее о конечной..."><strong>Южный вокзал</strong></a> - <a href="/streets/133/" title="Подробнее об улице...">ул.&nbsp;Евгения Котляра</a>, <a href="/streets/179/" title="Подробнее об улице...">ул.&nbsp;Полтавский Шлях</a>, <a href="/streets/40/" title="Подробнее об улице...">пл.&nbsp;Сергиевская</a>, <a href="/streets/41/" title="Подробнее об улице...">Павловская&nbsp;пл.</a>, <a href="/streets/39/" title="Подробнее об улице...">пл.&nbsp;Конституции</a>, <a href="/streets/56/" title="Подробнее об улице...">пр.&nbsp;Московский</a>, <a href="/streets/36/" title="Подробнее об улице...">пл.&nbsp;Защитников Украины</a>, <a href="/streets/120/" title="Подробнее об улице...">ул.&nbsp;Молочная</a>, <a href="/streets/176/" title="Подробнее об улице...">ул.&nbsp;Плехановская</a>, <a href="/streets/159/" title="Подробнее об улице...">ул.&nbsp;Морозова</a>, <a href="/streets/49/" title="Подробнее об улице...">пр.&nbsp;Героев Сталинграда</a> - <a href="/city_routes/ks/105/" title="Подробнее о конечной..."><strong>Ул. Одесская (Просп. Гагарина)</strong></a></td></tr>
-
-				<tr><th>путь следования:</th><td><?= $fs_separated ?></td></tr>
+				<tr><th>Путь следования:</th><td><?= $fs_separated ?></td></tr>
 				
-				<tr><th>обратный путь следования:</th><td><?= $bs_separated ?></td></tr>
+				<tr><th>Обратный путь следования:</th><td><?= $bs_separated ?></td></tr>
 				
-				<tr><th>Маршрут обслуживает: </th><td><a href="/transporters/67/" title="Подробнее о перевозчике...">Салтовское трамвайное депо</a></td></tr>
+				<tr><th>Маршрут обслуживает: </th><td><a href="/transporters/67/" title="Подробнее о перевозчике..."><?= $transport["firma"] ?></a></td></tr>
 				</tbody></table>
-	
+		
 </div>
 </body>
 </html>
+<?= $transport["rospisanie"] ?>
