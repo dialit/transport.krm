5b1b4a0917fdec98d6b9e7769b132f3c6a6ed6e1
Dimonmara <komarinskiy@gmail.com>
2017-07-25 20:19:50 +0300

    Обновил, исправил ошибки
    Добавил ссылки на остановки и структурировал код

--- public/routes.php
+++ public/routes.php
@@ -14,7 +14,7 @@
     }
     $mfront = explode(",",$front);
     $mback = explode(",",$back);
-    unset($rows);
+    unset($rows);//очистить переменную
     unset($row);
     
     //формируем перечень названий остановок по маршруту
@@ -27,7 +27,7 @@
     //var_dump($sumfront);
     unset($value);
     
-    $fs_separated = implode(", ",$sumfront);//строка с названиями остановок
+    //$fs_separated = implode(", ",$sumfront);//строка с названиями остановок
     //формируем перечень названий остановок по маршруту
     $i=0;
     foreach ($mback as $value){
@@ -38,45 +38,97 @@
         $i++;
     }
     unset($value);
-    $bs_separated = implode(", ",$sumback);//строка с названиями остановок
+    //$bs_separated = implode(", ",$sumback);//строка с названиями остановок
 ?> 
 
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title><?= $transport["type"] ?>.&nbsp; <?= $transport["n_marshr"] ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    	<title>
+    	   <?= $transport["type"] ?>.&nbsp; <?= $transport["n_marshr"] ?>
+    	</title>
 </head>
+    
 <body>
-
-
-		<div id="content">
-		     <h1 class="tit18">
-		        <?= $transport["type"] ?>&nbsp;№<?= $transport["n_marshr"] ?>
-		     </h1>
-		<p><strong>Описание</strong> |&nbsp;<a href="index.html" title="Вернуться на карту">Схема&nbsp;маршрута</a> |&nbsp;<a href="kniga.php?id=<?=$k?>&c=" title="Отзывы о сервисе на маршруте...">Книга жалоб и предложений</a> |&nbsp;<a href="/city_routes/193/news/" title="Посмотреть все новости, связанные с данным маршрутом...">Новости&nbsp;по&nbsp;теме</a></p>
-<div class="tc_main">
-	<table class="under_line" cellspacing="0" cellpadding="3">
-				<tbody><tr><th>Стоимость проезда:</th><td><?= $transport["cena"] ?></td></tr>
-				
-				<tr><th>Интервал движения:</th><td><?= $transport["int_dvij"] ?></td></tr>
-				
-				<tr><th>Дни работы:</th><td><?= $transport["rej_raboty"] ?></td></tr>
-				
-				<tr><th>Время работы:</th><td><?= $transport["vr_raboty"] ?></td></tr>
-				
-				<tr><th>Конечные станции:</th><td><?= $transport["nach_kon"] ?></td></tr>
-				
-				<tr><th>Путь следования:</th><td><?= $fs_separated ?></td></tr>
-				
-				<tr><th>Обратный путь следования:</th><td><?= $bs_separated ?></td></tr>
-				
-				<tr><th>Маршрут обслуживает: </th><td><?= $transport["firma"] ?></td></tr>
-				</tbody></table>
-				
-			
-
-		
-</div>
-	<iframe src="<?= $transport["rasp"] ?>" width="100%" height="70%"></iframe>
+    <div id="content">
+        <h1 class="tit18">
+    		<?= $transport["type"] ?>&nbsp;№<?= $transport["n_marshr"] ?>
+    	</h1>
+    </div>
+        <p>
+    		<strong>Описание</strong>&nbsp;|&nbsp;
+    		<a href="index.php" title="Вернуться на карту">Схема маршрута</a>&nbsp;|&nbsp;
+    		<a href="kniga.php?id=<?=$k?>&c=" title="Отзывы о сервисе на маршруте...">Книга жалоб и предложений</a>&nbsp;|&nbsp;
+    		<a href="news" title="Посмотреть все новости, связанные с данным маршрутом...">Новости по теме</a>
+    	</p>
+    <div class="tc_main">
+    	<table class="under_line" cellspacing="0" cellpadding="3">
+    		<tbody>
+    		    <tr>
+    		        <th>Стоимость проезда:</th>
+    		        <td><?= $transport["cena"] ?></td>
+    		    </tr>
+    			<tr>
+    			    <th>Интервал движения:</th>
+    			    <td><?= $transport["int_dvij"] ?></td>
+    			</tr>
+    				<tr>
+    				<th>Дни работы:</th>
+    				<td><?= $transport["rej_raboty"] ?></td>
+    			</tr>
+    			<tr>
+    				<th>Время работы:</th>
+    				<td><?= $transport["vr_raboty"] ?></td>
+    			</tr>
+    			<tr>
+    				<th>Конечные станции:</th>
+    				<td><?= $transport["nach_kon"] ?></td>
+    			</tr>
+    				<tr>
+    				<th>Путь следования:</th>
+    				<td>
+    				    <?php 
+    				       foreach ($sumfront as $value){
+    				           $idstops = query("SELECT `id` FROM `stops` WHERE `stops_name` = ?", $value);
+    				           $stops = $idstops[0]["id"];
+    				           //выводим html ссылки под названием остановки
+    				           print "<a href='$stops' title='Показать остановку'>";
+    				           //прописуем название остановки
+    				           echo "$value,";
+    				           print "</a> ";
+    				       }
+    				       unset($value);
+                        ?>
+                    </td>
+    			</tr>
+    				
+    			<tr>
+    				<th>Обратный путь следования:</th>
+    				<td>
+    				    <?php 
+    				       foreach ($sumback as $value){
+    				           $idstops = query("SELECT `id` FROM `stops` WHERE `stops_name` = ?", $value);
+    				           $stops = $idstops[0]["id"];
+    				           //выводим html ссылки под названием остановки
+    				           print "<a href='$stops' title='Показать остановку'>";
+    				           //прописуем название остановки
+    				           echo "$value,";
+    				           print "</a> ";
+    				       }
+    				       unset($value);
+                        ?>
+    				</td>
+    			</tr>
+    				
+    			<tr>
+    			    <th>Маршрут обслуживает: </th>
+    			    <td><?= $transport["firma"] ?></td>
+    			</tr>
+    		</tbody>
+    	</table>
+    </div>
+    <H2 ALIGN="center">Расписание</H2>
+    <!-- Всталяем табличку с расписанием в фрейм -->
+    	<iframe src="<?= $transport["rasp"] ?>" width="100%" height="70%"></iframe>
 </body>
 </html>
