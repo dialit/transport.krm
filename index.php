e24e43726e0ea53a28459f7fa01a29fac396a960
alexeysalnikov212 <futurist212@gmail.com>
2017-07-31 13:21:58 +0300

    исправления, на index добавил часы, надо оформить

--- index.php
+++ index.php
@@ -22,72 +22,66 @@
         <link href="css/styles.css" rel="stylesheet"/>
 
         <title>Транспорт Краматорска</title>
-        <!-- <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      .controls {
-        margin-top: 10px;
-        border: 1px solid transparent;
-        border-radius: 2px 0 0 2px;
-        box-sizing: border-box;
-        -moz-box-sizing: border-box;
-        height: 32px;
-        outline: none;
-        box-shadow: 0 2px 6px rgba(0, 0, 0, 0.3);
-      }
-
-      #pac-input {
-        background-color: #fff;
-        font-family: Roboto;
-        font-size: 15px;
-        font-weight: 250;
-        margin-left: 12px;
-        padding: 0 11px 0 13px;
-        text-overflow: ellipsis;
-        width: 250px;
-      }
-
-      #pac-input:focus {
-        border-color: #4d90fe;
-      }
-
-      .pac-container {
-        font-family: Roboto;
-      }
-
-      #type-selector {
-        color: #fff;
-        background-color: #4d90fe;
-        padding: 5px 11px 0px 11px;
-      }
-
-      #type-selector label {
-        font-family: Roboto;
-        font-size: 13px;
-        font-weight: 300;
-      }
-      #target {
-        width: 345px;
-      }
-    </style> -->
+        
     </head>
     
     <body class="cbp-spmenu-push">
-    <header>
-       
+    
+        <!-- Modal -->
+        <div id="myModalBox" class="modal fade">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true" aria-label="Закрыть">&times;</button>
+                        <h4 class="modal-title">Информация о маршруте</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div id="routeInfoContent"></div>
+                    </div>
+                    <div class="modal-footer">
+<!--                        <button type="button" class="btn btn-default" data-dismiss="modal">Закрыть</button>-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+       <!-- Feedback  Modal-->
+       <div id="feedback" class="modal fade feedback" tabindex="-1" role="dialog" aria-hidden="true" aria-labelledby="ModalLabel">
+           <div class="modal-dialog" role="document">
+               <div class="modal-content">
+                  <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Закрыть"><span aria-hidden="true">&times;</span></button>
+                      <h4 class="modal-title" id="gridSystemModalLabel">Форма обратной связи</h4>
+                  </div>
+                   <div class="modal-body">
+                       <div class="row">
+                           <div class="col-md-12">
+                               <div class="hide" id="respons"></div>
+                               <form method="POST" action="mail.php" id="mailForm">
+                                   <div class="form-group">
+                                       <label for="labelEmail">Email</label>
+                                       <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="labelPhone">Телефон</label>
+                                       <input type="text" class="form-control" name="mobile" id="mobile" placeholder="+38XXXXXXXXXX" required>
+                                   </div>
+                                   <div class="form-group">
+                                       <label for="labelText">Текст</label>
+                                       <textarea name="text" id="text" cols="30" rows="10" class="form-control"></textarea>
+                                   </div>
+                                   <button type="submit" class="btn btn-primary">Отправить</button>
+                               </form>
+                           </div>
+                       </div>
+                   </div><!-- End of Modal body -->
+               </div><!-- End of Modal content -->
+           </div><!-- End of Modal dialog -->
+       </div><!-- End of Modal -->
+        
+        <header>       
         <nav class="navbar navbar-default">
-           
-            <div class="container">
-               
+             <div class="container">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
                         <span class="sr-only">Переключение навигации</span>
@@ -95,23 +89,42 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#"><span>Транспорт Краматорска</span></a>
+                    <a class="navbar-brand" href="/"><span>Транспорт Краматорска</span></a>
                 </div>
                 
                 <div class="collapse navbar-collapse" id="navbar-collapse">
                     <ul class="nav navbar-nav">
-                        <li><a id="showLeft">Маршруты</a></li>
+                        <li><button class="btn btn-default" id="showLeft">Маршруты</button></li>
 <!--                        <li><a id="showRight">Link</a></li>-->
                     </ul>
                     <form class="navbar-form navbar-left" id="form" role="form">
                         <div class="form-group">
-                            <input id="pac-input" class="form-controls" type="text" placeholder="Поиск адреса">
+                            <input type="text" id="pac-input" class="form-control" placeholder="Поиск адреса">
                             <input type="text" id="q" class="form-control" placeholder="Поиск остановки">
-                            <a href="javascript:n_stops_chn();" class="button btn btn-default" id="button" type="button">Сброс маршрутов</a>
+                                <a href="javascript:n_stops_chn();" class="button btn btn-default btn-xs" id="buttonReset" type="button" onclick="resetMarshr()">Сброс</a>
+                                <a href="javascript:infoGeoFind();" class="button btn btn-default btn-xs" id="button" type="button">GPS</a>
+                                
+                                
+                                <script language="JavaScript">
+                                    dayarray=new Array("воскресенье","понедельник","вторник","среда","четверг","пятница","суббота")
+                                    montharray=new Array ("января","февраля","марта","апреля","мая","июня","июля","августа","сентября","октября","ноября","декабря")
+                                    var ndata=new Date();
+                                    var day=dayarray[ndata.getDay()];
+                                    var month=montharray[ndata.getMonth()];
+                                    var date=ndata.getDate();
+                                    var year=ndata.getYear()-100;
+                                    datastr=("Сегодня "+ date +" "+ month +" 20"+ year +" года, "+day+"." )
+                                </script>
+                                <span>Сегодня: <script language="JavaScript">document.write(datastr);</script></span>
+                                <span>Время: <div id="timedisplay"></div></span>
+                                
+<!--                                <span>Сегодня: <? echo date('d.m.Y H:i'); ?></span>  -->
+                        
+                        
                         </div>
                     </form>
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#">Обратная связь</a></li>
+                        <li><button class="btn btn-default" data-toggle="modal" data-target=".feedback">Обратная связь</button></li>
                     </ul>
                 </div>
             </div>
@@ -119,7 +132,12 @@
     </header>
     
     <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
-        <h3>Выберите вид транспорта</h3><a id="showLeft" title="Свернуть"><i class="glyphicon glyphicon-chevron-left" aria-hidden="true"></i></a>
+       <div class="">
+            <a class="off" id="showLeft1" title="Свернуть">
+            <i class="glyphicon glyphicon-chevron-left" aria-hidden="true"></i>
+            </a>
+            <h3>Выберите вид транспорта</h3>
+       </div>
             <div class="panel-group" id="accordion">
                 
                 <!-- Маршрутки -->
@@ -133,16 +151,19 @@
                     <div id="collapse-1" class="panel-collapse collapse in">
                         <!-- Содержимое Маршруток -->
                         <div class="panel-body">
-                            <ul class="list-inline">
-                               <?php
-                                    $taxies = query("SELECT id, n_marshr FROM `transport` WHERE type = 'Маршрутное такси'");
-                                    foreach ($taxies as $taxi) {
-                                        $id = $taxi["id"];
-                                        $ntaxi = $taxi["n_marshr"];    
-                                        echo "<li><a href=\"javascript:draw_marshr(3,$id)\" title=\"$ntaxi\">$ntaxi</a></li>";
-                                    }
-                                ?>
-                            </ul>
+                            <div id="ButtonsNmarshr">    
+                                <ul class="list-inline">
+                                <?php
+                                        $taxies = query("SELECT `id`,`n_marshr`,`nach_kon` FROM `transport` WHERE type = 'Маршрутное такси'");
+                                        foreach ($taxies as $taxi) {
+                                            $id = $taxi["id"];
+                                            $ntaxi = $taxi["n_marshr"];
+                                            $t = $taxi["nach_kon"];
+                                            echo "<li><a role=\"button\" data-toggle=\"tooltip\" title=\"$t\" class=\"btn btn-default\" href=\"javascript:draw_marshr(3,$id)\" >$ntaxi</a></li>";
+                                        }
+                                    ?>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -155,20 +176,22 @@
                             <a data-toggle="collapse" data-parent="#accordion" href="#collapse-2">Автобусы</a>
                         </h4>
                     </div>
-                    <div id="collapse-2" class="panel-collapse collapse in">
+                    <div id="collapse-2" class="panel-collapse collapse">
                         <!-- Содержимое Автобусов -->
                         <div class="panel-body">
-                            <ul class="list-inline">
-                               <?php
-                                    $buses = query("SELECT id, n_marshr FROM `transport` WHERE type = 'Автобус'");
-                                    foreach ($buses as $bus) {
-                                        $id = $bus["id"];
-                                        $nbus = $bus["n_marshr"];    
-                                        echo "<li><a href=\"javascript:draw_marshr(3,$id)\" title=\"$nbus\">$nbus</a></li>";
-                                        
-                                    }
-                                ?>
-                            </ul>
+                            <div id="ButtonsNmarshr">
+                                <ul class="list-inline">
+                                <?php
+                                        $buses = query("SELECT `id`,`n_marshr`,`nach_kon` FROM `transport` WHERE type = 'Автобус'");
+                                        foreach ($buses as $bus) {
+                                            $id = $bus["id"];
+                                            $nbus = $bus["n_marshr"];
+                                            $t = $taxi["nach_kon"];
+                                            echo "<li><a role=\"button\" data-toggle=\"tooltip\" title=\"$t\" class=\"btn btn-default\" href=\"javascript:draw_marshr(3,$id)\" title=\"$nbus\">$nbus</a></li>";
+                                        }
+                                    ?>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -181,38 +204,63 @@
                             <a data-toggle="collapse" data-parent="#accordion" href="#collapse-3">Троллейбусы</a>
                         </h4>
                     </div>
-                    <div id="collapse-3" class="panel-collapse collapse in">
+                    <div id="collapse-3" class="panel-collapse collapse">
                         <!-- Содержимое Троллейбусов -->
                         <div class="panel-body">
-                            <ul class="list-inline">
-                               <?php
-                                    $tbuses = query("SELECT id, n_marshr FROM `transport` WHERE type = 'Троллейбус'");
-                                    foreach ($tbuses as $tbus) {
-                                        $id = $tbus["id"];
-                                        $ntbus = $tbus["n_marshr"];    
-                                        echo "<li><a href=\"javascript:draw_marshr(3,$id)\" title=\"$ntbus\">$ntbus</a></li>";
-                                    }
-                                ?>
-                            </ul>
+                            <div id="ButtonsNmarshr">
+                                <ul class="list-inline">
+                                <?php
+                                        $tbuses = query("SELECT `id`,`n_marshr`,`nach_kon` FROM `transport` WHERE type = 'Троллейбус'");
+                                        foreach ($tbuses as $tbus) {
+                                            $id = $tbus["id"];
+                                            $ntbus = $tbus["n_marshr"];
+                                            $t = $taxi["nach_kon"];    
+                                            echo "<li><a role=\"button\" data-toggle=\"tooltip\" title=\"$t\" class=\"btn btn-default\" href=\"javascript:draw_marshr(3,$id)\" title=\"$ntbus\">$ntbus</a></li>";
+                                        }
+                                    ?>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- Трамваи -->
+                <div class="panel panel-default">
+                    <!-- Заголовок -->
+                    <div class="panel-heading">
+                        <h4 class="panel-title text-center">
+                            <a data-toggle="collapse" data-parent="#accordion" href="#collapse-4">Трамваи</a>
+                        </h4>
+                    </div>
+                    <div id="collapse-4" class="panel-collapse collapse">
+                        <!-- Содержимое Трамваев -->
+                        <div class="panel-body">
+                            <div id="ButtonsNmarshr">
+                                <ul class="list-inline">
+                                <?php
+                                        $trams = query("SELECT `id`,`n_marshr`,`nach_kon` FROM `transport` WHERE type = 'Трамвай'");
+                                        foreach ($trams as $tram) {
+                                            $id = $tram["id"];
+                                            $ntram = $tram["n_marshr"];
+                                            $t = $taxi["nach_kon"];    
+                                            echo "<li><a role=\"button\" data-toggle=\"tooltip\" title=\"$t\" class=\"btn btn-default\" href=\"javascript:draw_marshr(3,$id)\" title=\"$ntram\">$ntram</a></li>";
+                                        }
+                                    ?>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
     </nav>
-    
-    <nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-right" id="cbp-spmenu-s2">
-        <h3>Информация о маршруте</h3>
-        <?php var_dump($right); ?>
-    </nav>
-
-
-        <!-- fill viewport -->
-        <div class="container">
+            <!-- fill viewport -->
+        <div class="container-map">
             
              <!-- https://developers.google.com/maps/documentation/javascript/tutorial -->
-            <div id="map-canvas"></div>               
-
+            <div id="map-canvas" class="height: 90%"></div>               
         </div>
+        
+        <footer></footer>
 
         <!-- https://developers.google.com/maps/documentation/javascript/ -->
         <script src="https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyDddUMqAgSPHOym9KhggEoONdiHPQwUxpE&libraries=places"></script>
@@ -225,6 +273,9 @@
 
         <!-- http://getbootstrap.com/ -->
         <script src="js/bootstrap.min.js"></script>
+        
+        <!-- Подключения скрипта control-modal.js к странице -->
+<!--        <script src="js/control-modal.js"></script>-->
 
         <!-- http://underscorejs.org/ -->
         <script src="js/underscore-min.js"></script>
@@ -239,9 +290,33 @@
 				
 		<!-- Slide Push Menu JavaScript -->
 		<script src="js/spmenu.js"></script>
+		
+        <!-- Feedback JavaScript -->
+		<script src="js/feedback.js"></script>
 
         <!-- app's own JavaScript -->
         <script src="js/scripts.js"></script>
-   
+
+        <script language="JavaScript">
+            function getDate()
+            {
+            var date = new Date();
+            var hours = date.getHours();
+            var minutes = date.getMinutes();
+            var seconds = date.getSeconds();
+            if(hours < 10){
+            hours = '0' + hours;
+            }
+            if(minutes < 10){
+            minutes = '0' + minutes;
+            }
+            if(seconds < 10){
+            seconds = '0' + seconds;
+            }
+            document.getElementById('timedisplay').innerHTML = hours + ':' + minutes + ':' + seconds;
+            }
+            setInterval(getDate, 0);
+        </script>
+        
     </body>
 </html>
---
+++ routes.php
@@ -0,0 +1,112 @@
+<?php
+
+    // конфигурация
+    require("../includes/config.php"); 
+
+    // запрашивать базу данных transport для пользователя
+    $k = str_replace('\'','',$_GET["id"] );
+    $rows = query("SELECT `id`,`type`,`n_marshr`,`nach_kon`,`cena`,`int_dvij`,`rej_raboty`,`vr_raboty`,`front`,`back`,`firma`,`rasp` FROM `transport` WHERE `id` = ?",$k);
+    
+    //var_dump($k);
+    $transport = $rows[0];
+    $front = $rows[0]["front"];//список id остановок по маршруту
+    //var_dump($front);
+    $back = $rows[0]["back"];//список id остановок по маршруту в обратном направлении
+    //var_dump($back);
+    
+    //создаём массив из front  
+    $tok = strtok($front, ",");
+    $mas_front = [];
+    while ($tok !== false) 
+    {
+        $mas_front[]=$tok;
+        $tok = strtok(",");
+    }
+    //создаём массив из back  
+    $tok = strtok($back, ",");
+    $mas_back = [];
+    while ($tok !== false) 
+    {
+        $mas_back[]=$tok;
+        $tok = strtok(",");
+    }
+    //var_dump($mas_front);
+    //var_dump($mas_back);
+    
+?> 
+
+<div class="container-fluid">
+       <div class="row">
+           <div class="col-md-12">
+               <h3><?= $transport["type"] ?>&nbsp;№<?= $transport["n_marshr"] ?></h3>
+               <p><strong>Описание</strong>&nbsp;|&nbsp;
+                <a href='javascript:draw_marshr(3,<?=$k?>)' title="Вернуться на карту">Схема маршрута</a>&nbsp;|&nbsp;
+                <a href="kniga.php?id=<?=$k?>&c=" title="Отзывы о сервисе на маршруте...">Книга жалоб и предложений</a>&nbsp;|&nbsp;
+                <a href="news" title="Посмотреть все новости, связанные с данным маршрутом...">Новости по теме</a></p>
+           </div>
+       </div>
+       <div class="row">
+           <div class="col-md-12">
+           <table class="table table-striped table-bordered table-hover">
+              <thead>
+                  <th>Стоимость проезда:</th>
+                  <th>Интервал движения:</th>
+                  <th>Дни работы:</th>
+                  <th>Время работы:</th>
+                  <th>Конечные остановки:</th>
+              </thead>
+               <tbody>
+                   <tr>
+                       <td><?= $transport["cena"] ?></td>
+                       <td><?= $transport["int_dvij"] ?></td>
+                       <td><?= $transport["rej_raboty"] ?></td>
+                       <td><?= $transport["vr_raboty"] ?></td>
+                       <td><?= $transport["nach_kon"] ?></td>
+                   </tr>
+               </tbody>
+           </table>
+            <table class="table table-striped table-bordered table-hover">
+                <thead>
+                    <th>Путь следования:</th>
+                    <th>Обратный путь:</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                                <?php 
+                                    //перебираем массив с id остановок и запрашиваем по ним названия остановок из базы
+                                    //var_dump($front);
+                                    foreach ($mas_front as $value){
+	    				                //var_dump($value);
+                                        $namestops = query("SELECT `id`,`stops_name` FROM `stops` WHERE `id` = ?", $value);
+	    				                //var_dump($namestops);
+                                        $id = $namestops[0]["id"];
+                                        $names = $namestops[0]["stops_name"];
+	                                    //выводим названия остановок и html ссылки под названием остановки c id остановок
+	                                    echo "<a href='javascript:update(5, $id)' title='Показать остановку'>$names,</a>&ensp;";
+                                    }
+                                    unset($value);
+                                ?>
+                       </td>
+                       <td>
+                                <?php 
+                                    //перебираем массив с id остановок и запрашиваем по ним названия остановок из базы
+                                    foreach ($mas_back as $value){
+	    				                $namestops = query("SELECT `id`,`stops_name` FROM `stops` WHERE `id` = ?", $value);
+	    				                $id = $namestops[0]["id"];
+	                                    $names = $namestops[0]["stops_name"];
+                                        //выводим названия остановок и html ссылки под названием остановки c id остановок
+	                                    echo "<a href='javascript:update(5, $id)' title='Показать остановку'>$names,</a>&ensp;";
+                                    }
+                                    unset($value);
+                                ?>
+                       </td>
+                    </tr>
+                </tbody>
+            </table>
+            <p>Маршрут обслуживает:<?= $transport["firma"] ?></p>            
+            <h3>Расписание</h3>
+            <iframe src="<?= $transport["rasp"] ?>" frameborder="0" width="100%" height="100%"></iframe>
+        </div>
+    </div>
+</div>
